5f9e6d309339924d5e232fb9ec69744df11871c1
nacong <baskinnabeens@gmail.com>
2026-01-17 18:13:47 +0900

    chore: 대시보드 메인 페이지의 제비앱 사진 추가함.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,12 +1,11 @@
-import { Suspense } from 'react';
- 
 export default async function Page() {
   
   return (
     <main>
       <h1 className={`mb-4 text-xl md:text-2xl`}>
-        Dashboard
+        제비 대시보드
       </h1>
+      <img src="/zebi-app.png" alt="제비 앱 이미지" width="280"/>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
       </div>
     </main>
